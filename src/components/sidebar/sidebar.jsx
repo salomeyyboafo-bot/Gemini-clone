6fb316bd63e12d706a39b88617ed05c479a59e43
Salomey Boafo <salomeyyboafo@gmail.com>
2025-11-24 15:30:55 +0000

    22.16

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -7,16 +7,18 @@ const Sidebar = () => {
     <div className ='sidebar' >
       <div className='top'>
         <img className='Menu' src={assets.menu_icon} alt = "" />
-        <div className='new-chat'></div>
+        <div className='new-chat'> 
            <img src={assets.plus_icon} alt="" />
-        <p>New Chat</p>
+           <p>New Chat</p>
       </div>
       <div className="recent">
         <p className="recent-title">Recent</p>
         <div className="recent-entry">
           <img src={assets.message_icon} alt="" />
           <p>What is react...</p>
-      </div>
+
+            </div>
+         </div>
       </div>
       
 
