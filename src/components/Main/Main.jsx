f8aa56259c91b83d151e4b56d14d7b7a37f9d2bf
Salomey Boafo <salomeyyboafo@gmail.com>
2025-11-27 11:15:39 +0000

    main.css

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -15,25 +15,40 @@ const Main = () => {
                 <p>How Can I help You Today?</p>
             </div>
             <div className="cards">
+                <div className="card">
                 <p>Suggest beautiful places to see on an upcoming road trip</p>
                 <img src={assets.compass_icon} alt="" />
             </div>
-            <div className="cards">
+            <div className="card">
                 <p>Briefly summarize this concept:urban planning</p>
                 <img src={assets.bulb_icon} alt="" />
             </div>
-            <div className="cards">
-                <p>SBrainstorm team bonding activities for our work retreat</p>
+            <div className="card">
+                <p>Brainstorm team bonding activities for our work retreat</p>
                 <img src={assets.message_icon} alt="" />
             </div>
-            <div className="cards">
+            <div className="card">
                 <p>Improve the readability of the following code</p>
                 <img src={assets.code_icon} alt="" />
             </div>
         </div>
-
+        <div className="main-bottom">
+            <div className="search-box">
+                <input type="text" placeholder='Enter a propmt here'/>
+            </div>
+            <img src={assets.gallery_icon} alt="" />
+            <img scr={assets.mic_icon} alt=""/>
+            <img src={assets.send_icon} alt=""/>
+        </div>
+      </div>
+      <p className="bottom-info">
+        'elloBaE may display inaccurate info, including about people, so double check its response.Your privicy and 'elloBaE Apps
+      </p>
     </div>
+        
   )
+  
+   
 }
 
 export default Main
